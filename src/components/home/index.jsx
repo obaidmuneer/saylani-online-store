535d61b8f6e385ecdbdfbae6be0157298be4191d
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-16 19:47:56 +0500

    fetch category and products

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -5,12 +5,14 @@ import {
     Text,
 } from '@chakra-ui/react';
 import { GlobalContext } from '../../context/context';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import ProductCard from '../ui-component/productCard';
 import ProductSearch from '../productSearch';
 import CategoryCard from '../ui-component/categoryCard';
 import Carousel from 'react-multi-carousel';
 import "react-multi-carousel/lib/styles.css";
+import useCategory from '../../hooks/useCategory';
+import useProducts from '../../hooks/useProduct';
 
 
 
@@ -22,14 +24,14 @@ const responsive = {
     },
     desktop: {
         breakpoint: { max: 3000, min: 1024 },
-        items: 4
+        items: 5
     },
     tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 3
+        breakpoint: { max: 1024, min: 636 },
+        items: 4
     },
     mobile: {
-        breakpoint: { max: 464, min: 0 },
+        breakpoint: { max: 636, min: 0 },
         items: 3
     }
 };
@@ -37,10 +39,21 @@ const responsive = {
 
 const Home = () => {
     const { state } = useContext(GlobalContext)
+    const { getCategory } = useCategory()
+    const { getProducts } = useProducts()
+
+    useEffect(() => {
+        (async () => {
+            await getProducts()
+            await getCategory()
+        })();
+    }, [])
+
     return (
         <div>
             <Container maxW={'5xl'}>
-                <Stack>
+                <Stack spacing={5} my={5}
+                >
                     <Image
                         src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
                         alt='Green double couch with wooden legs'
@@ -52,31 +65,27 @@ const Home = () => {
                         Shop by Category
                     </Text>
                     <Carousel responsive={responsive}>
-                        <CategoryCard title={'sofa'} img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'} />
-                        <CategoryCard title={'sofa'} img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'} />
-                        <CategoryCard title={'sofa'} img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'} />
-                        <CategoryCard title={'sofa'} img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'} />
-                        <CategoryCard title={'sofa'} img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'} />
-                        <CategoryCard title={'sofa'} img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'} />
-                        <CategoryCard title={'sofa'} img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'} />
+                        {
+                            state.category.map(category => {
+                                return <CategoryCard key={category._id} title={category.title} img={category.file} />
+                            })
+                        }
                     </Carousel>
 
-
-                    <ProductCard img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'}
-                        title='this is title'
-                        unit_name={'kg'}
-                        unit_price={800}
-                        desc={'this is desc for product'} />
-                    <ProductCard img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'}
-                        title='this is title'
-                        unit_name={'kg'}
-                        unit_price={800}
-                        desc={'this is desc for product'} />
-                    <ProductCard img={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'}
-                        title='this is title'
-                        unit_name={'kg'}
-                        unit_price={800}
-                        desc={'this is desc for product'} />
+                    {
+                        state.products.length > 0 ? state.products.map(product => {
+                            return <ProductCard
+                                key={product._id}
+                                img={product.file}
+                                title={product.title}
+                                unit_name={product.unit_name}
+                                unit_price={product.unit_price}
+                                desc={product.description} />
+                        }) :
+                            <Text>
+                                Product Not Found
+                            </Text>
+                    }
                 </Stack>
             </Container>
         </div >
