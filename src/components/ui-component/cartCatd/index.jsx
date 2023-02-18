2bcab6749edce8b82118fc1eaf261c66e90f354a
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-19 00:08:13 +0500

    CartCard comp

--- src/components/ui-component/cartCatd/index.jsx
+++ src/components/ui-component/cartCatd/index.jsx
@@ -2,32 +2,46 @@ import { Card, CardHeader, CardBody, CardFooter, Text, Button, Heading, Image, S
 import { IoIosAdd, } from 'react-icons/io'
 import { FiMinus } from 'react-icons/fi'
 import CIcon from '../CIcon'
-const CartCard = () => {
+import { useContext, useState } from 'react'
+import { GlobalContext } from '../../../context/context'
+
+const CartCard = ({ title, unit_price, img }) => {
+    const [quantity, setQuantity] = useState(1)
+    const { state, dispatch } = useContext(GlobalContext)
+
+    const handlePrice = () => {
+        dispatch({
+            type: 'order',
+            payload: [...state.orders]
+        })
+    }
     return (
         <Card
             direction={{ base: 'column', sm: 'row' }}
             overflow='hidden'
-            // variant='outline'
             h={150}
         >
             <Image
-            rounded={'lg'}
+                rounded={'lg'}
                 objectFit='cover'
                 maxW={{ base: '100%', sm: '200px' }}
-                src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-                alt='Caffe Latte'
+                src={img}
+                alt='product image'
             />
 
             <HStack width={'full'} px={2} >
                 <CardBody >
-                    <Heading size='md'>Item Name</Heading>
+                    <Heading size='md'>{title}</Heading>
+                    <Text>Product Price : {unit_price}</Text>
 
                     <HStack py='5' >
-                        <CIcon>
+                        <CIcon handleClick={() => setQuantity(quantity + 1)} >
                             <IoIosAdd size={26} />
                         </CIcon>
-                        <Badge fontSize={16} py={3} px={5} rounded={'lg'} >2</Badge>
-                        <CIcon>
+                        <Badge fontSize={16} py={3} px={5} rounded={'lg'} >{
+                            quantity
+                        }</Badge>
+                        <CIcon handleClick={() => quantity <= 1 ? null : setQuantity(quantity - 1)} >
                             <FiMinus size={18} />
                         </CIcon>
                     </HStack>
@@ -36,7 +50,7 @@ const CartCard = () => {
                 <Spacer />
 
                 <Text fontSize={20} >
-                    Price : 12
+                    Price : {quantity * unit_price}
                 </Text>
             </HStack>
         </Card>
