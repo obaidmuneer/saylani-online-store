fce29c5b6dd5388d1f1075bf0157da95b79aa8e3
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-21 22:22:00 +0500

    added dlt cart, checkout apis & some validation

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -2,11 +2,13 @@ import { Box, Button, Heading, HStack, Spacer, Stack, Text } from '@chakra-ui/re
 import { useFormik } from 'formik'
 import * as yup from 'yup';
 import { MdDelete } from 'react-icons/md'
-import CartCard from '../ui-component/cartCatd'
+import CartCard from '../ui-component/cartCard'
 import FormikForm from '../formikForm';
 import FormikInput from '../formikInput';
 import { useContext } from 'react';
 import { GlobalContext } from '../../context/context';
+import useCart from '../../hooks/useCart';
+import useOrder from '../../hooks/useOrder';
 
 const validationSchema = yup.object().shape({
     name: yup.string().min(3).required('Please Enter Your Name'),
@@ -15,6 +17,8 @@ const validationSchema = yup.object().shape({
 
 const Cart = () => {
     const { state } = useContext(GlobalContext)
+    const { deleteCart, isLoading } = useCart()
+    const { placeOrder } = useOrder()
 
     const formik = useFormik({
         initialValues: {
@@ -23,7 +27,9 @@ const Cart = () => {
         },
         validationSchema: validationSchema,
         onSubmit: async (values, actions) => {
-            console.log(values);
+            // console.log(values);
+            await placeOrder(values)
+            actions.setSubmitting(false)
         },
     });
     return (
@@ -34,44 +40,59 @@ const Cart = () => {
                     <Text color={'blue.400'} >Cart</Text>
                 </Heading>
                 <Spacer />
-                <Button color={'green.400'} rounded='full' p={0} >
-                    <MdDelete size={26} />
-                </Button>
-            </HStack>
-            {/* title, unit_price, img */}
-            <Stack>
                 {
-                    state?.cart?.orders.map(order => <CartCard key={order.product._id} title={order.product.title}
-                        unit_price={order.product.unit_price}
-                        img={order.product.file} />)
+                    state?.cart?.isChecked === false &&
+                    <Button onClick={deleteCart} isLoading={isLoading} color={'green.400'} rounded='full' p={0} >
+                        <MdDelete size={26} />
+                    </Button>
                 }
-            </Stack>
-            <HStack fontSize={20} fontWeight={600} my={2} >
-                <Text  >
-                    Shopping
-                </Text>
-                <Spacer />
-                <Text >Total Price : {state?.cart?.total}</Text>
+
             </HStack>
+            {
+                state?.cart?.isChecked === false ? <>
+                    <Stack>
+                        {
+                            state?.cart?.orders?.map(order => <CartCard key={order.product._id} title={order.product.title}
+                                unit_price={order.product.unit_price}
+                                img={order.product.file}
+                                total_price={order.total}
+                                quantity={order.quantity}
+                                id={order.product._id}
+                            />)
+                        }
+                    </Stack>
+                    <HStack fontSize={20} fontWeight={600} my={2} >
+                        <Text  >
+                            Shopping
+                        </Text>
+                        <Spacer />
+                        <Text >Total Price : {state?.cart?.total}</Text>
+                    </HStack>
 
-
-            <FormikForm
-                formik={formik}
-                _dir={'column'}
-                color={'green'}
-                btnTitle={"Place Order"}
-            >
-                <FormikInput
-                    formik={formik}
-                    nameLabel={'name'}
-                    placeHolder={'Enter Your Name'} />
-                <FormikInput
-                    formik={formik}
-                    nameLabel={'address'}
-                    placeHolder={'Enter Address for parcel'} />
-            </FormikForm>
-
-
+                    <FormikForm
+                        formik={formik}
+                        _dir={'column'}
+                        color={'green'}
+                        btnTitle={"Place Order"}
+                    >
+                        <FormikInput
+                            formik={formik}
+                            nameLabel={'name'}
+                            placeHolder={'Enter Your Name'} />
+                        <FormikInput
+                            formik={formik}
+                            nameLabel={'address'}
+                            placeHolder={'Enter Address for parcel'} />
+                    </FormikForm>
+                </> : 'Please order product from home page to see cart '
+            }
+            <br />
+            {
+                state.orders.length ?
+                    state.orders.map(order => order.status == 'pending' ?
+                        `You have ${state.orders.length} orders in pending` : null)
+                    : null
+            }
         </Box>
     )
 }
