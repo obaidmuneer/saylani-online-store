e606186d5838fab14f22f3da68e1ba819a329c46
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-21 22:23:40 +0500

    added update & dlt cart api

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -11,29 +11,93 @@ const useCart = () => {
     const addToCart = async (id) => {
         setIsLoading(true)
         // console.log(id);
-        const res = await axios.post(`${state.api}orders`, {
-            product_id: id,
-            quantity: 1,
-            cart_id: state?.cart?._id
-        }, { withCredentials: true })
-        // console.log(res.data.cart);
-        dispatch({
-            type: 'cart',
-            payload: res.data.cart
-        })
-        toast({
-            title: res.data.messege.text,
-            status: res.data.messege.type,
-            position: 'bottom-right',
-            // containerStyle: {
-            //     mb: "70px"
-            // },
-            isClosable: true,
-        })
+        try {
+            const res = await axios.post(`${state.api}orders`, {
+                product_id: id,
+                quantity: 1,
+                cart_id: state?.cart?._id
+            }, { withCredentials: true })
+            // console.log(res.data.cart);
+            dispatch({
+                type: 'cart',
+                payload: res.data.cart
+            })
+            toast({
+                title: res.data.messege.text,
+                status: res.data.messege.type,
+                position: 'bottom-right',
+                isClosable: true,
+            })
+        } catch (error) {
+            toast({
+                title: 'Something went wrong',
+                status: 'error',
+                position: 'bottom-right',
+                isClosable: true,
+            })
+        }
         setIsLoading(false)
     }
 
-    return { addToCart, isLoading }
+    const updateCart = async (id, qty) => {
+        setIsLoading(true)
+        try {
+            const res = await axios.put(`${state.api}orders`, {
+                cart_id: state?.cart?._id,
+                product_id: id,
+                quantity: qty,
+            }, { withCredentials: true })
+            // console.log(res.data);
+            dispatch({
+                type: 'cart',
+                payload: res.data.cart
+            })
+            toast({
+                title: res.data.messege.text,
+                status: res.data.messege.type,
+                position: 'bottom-right',
+                isClosable: true,
+            })
+        } catch (error) {
+            // console.log(error.response.data);
+            toast({
+                title: error.response.data.messege.text,
+                status: error.response.data.messege.type,
+                position: 'bottom-right',
+                isClosable: true,
+            })
+        }
+        setIsLoading(false)
+    }
+
+    const deleteCart = async () => {
+        setIsLoading(true)
+        try {
+            const res = await axios.delete(`${state.api}orders/${state?.cart?._id}`, { withCredentials: true })
+            console.log(res.data);
+            dispatch({
+                type: 'cart',
+                payload: []
+            })
+            toast({
+                title: res.data.messege.text,
+                status: res.data.messege.type,
+                position: 'bottom-right',
+                isClosable: true,
+            })
+        } catch (error) {
+            // console.log(error.response.data);
+            toast({
+                title: error.response.data.messege.text,
+                status: error.response.data.messege.type,
+                position: 'bottom-right',
+                isClosable: true,
+            })
+        }
+        setIsLoading(false)
+    }
+
+    return { addToCart, updateCart, deleteCart, isLoading }
 }
 
 export default useCart
