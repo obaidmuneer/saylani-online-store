29d6613c714b503afcb3a074f88f10c6d7d4da0a
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-23 23:28:41 +0500

    configured orders api

--- src/hooks/useOrder.js
+++ src/hooks/useOrder.js
@@ -6,21 +6,46 @@ import { GlobalContext } from "../context/context";
 const useOrder = () => {
     const { state, dispatch } = useContext(GlobalContext)
     const [isLoading, setIsLoading] = useState(false)
+    const [sucess, setSucess] = useState(false)
     const toast = useToast()
+
+    const getOrders = async () => {
+        setIsLoading(true)
+        try {
+            const res = await axios.get(`${state.api}orders`, { withCredentials: true })
+            // console.log(res.data.orders);
+            dispatch({
+                type: 'adminOrders',
+                payload: res.data.orders
+            })
+        } catch (error) {
+            toast({
+                title: 'Something went wrong',
+                status: 'error',
+                position: 'bottom-right',
+                isClosable: true,
+            })
+        }
+        setIsLoading(false)
+    }
 
     const placeOrder = async ({ name, address }) => {
         setIsLoading(true)
         try {
-            const res = await axios.post(`${state.api}orders/place`, {
+            const res = await axios.post(`${state.api}orders`, {
                 cart_id: state?.cart?._id,
                 name,
                 address,
             }, { withCredentials: true })
             console.log(res.data);
-            // dispatch({
-            //     type: 'cart',
-            //     payload: res.data.cart
-            // })
+            dispatch({
+                type: 'cart',
+                payload: []
+            })
+            dispatch({
+                type: 'orders',
+                payload: [res.data.order, ...state.orders]
+            })
             toast({
                 title: res.data.messege.text,
                 status: res.data.messege.type,
@@ -38,9 +63,37 @@ const useOrder = () => {
         setIsLoading(false)
     }
 
+    const updateOrder = async (id, status) => {
+        setIsLoading(true)
+        try {
+            // const indexItem = state.adminOrders.findIndex(order => order._id === id)
+            // console.log(indexItem);
+            const res = await axios.put(`${state.api}orders`, {
+                order_id: id,
+                status,
+            }, { withCredentials: true })
+            // console.log(res.data);
+            toast({
+                title: res.data.messege.text,
+                status: res.data.messege.type,
+                position: 'bottom-right',
+                isClosable: true,
+            })
+            setSucess(true)
+        } catch (error) {
+            toast({
+                title: 'Something went wrong',
+                status: 'error',
+                position: 'bottom-right',
+                isClosable: true,
+            })
+        }
+        setIsLoading(false)
+    }
 
 
-    return { placeOrder, isLoading }
+
+    return { getOrders, placeOrder, updateOrder, isLoading, sucess }
 }
 
 export default useOrder
