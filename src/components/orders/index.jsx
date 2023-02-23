d9977d903a8f497ed0edf8289695f7f94ef4c094
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-23 23:29:34 +0500

    conditonal ui for admin

--- src/components/orders/index.jsx
+++ src/components/orders/index.jsx
@@ -1,4 +1,4 @@
-import{ useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { Avatar, Box, Center, Divider, Heading, Stack } from '@chakra-ui/react'
 import { useFormik } from 'formik'
 import * as yup from 'yup';
@@ -7,6 +7,7 @@ import { GlobalContext } from '../../context/context'
 import FormikForm from '../formikForm'
 import FormikInput from '../formikInput'
 import COrderCard from '../ui-component/COrderCard';
+import useOrder from '../../hooks/useOrder';
 
 const validationSchema = yup.object().shape({
     name: yup.string().min(3).required('Please Enter Your Name'),
@@ -14,7 +15,13 @@ const validationSchema = yup.object().shape({
 
 const Orders = () => {
     const { state } = useContext(GlobalContext)
-    console.log(state.orders);
+    const { getOrders } = useOrder()
+
+    useEffect(() => {
+        if (state.user.isAdmin) {
+            (async () => await getOrders())()
+        }
+    }, [])
 
     const formik = useFormik({
         initialValues: {
@@ -30,38 +37,47 @@ const Orders = () => {
     return (
         <Center>
             <Stack alignItems='center'  >
-                <Heading color={'blue.500'} fontSize={'3xl'} >
-                    Setting
-                </Heading>
-                <Box border={'solid'} p={2} borderRadius={'full'} borderColor={'green.400'}>
-                    <Avatar
-                        size={'2xl'}
-                        src={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'}
-                    />
-                </Box>
-                <Box>
-                    <FormikForm
-                        formik={formik}
-                        color={'green'}
-                        btnTitle={"Update"}
-                    >
-                        <FormikInput
+                {!state.user.isAdmin && (
+                    <>
+                        <Heading color={'blue.500'} fontSize={'3xl'} >
+                            Setting
+                        </Heading>
+                        <Box border={'solid'} p={2} borderRadius={'full'} borderColor={'green.400'}>
+                            <Avatar
+                                size={'2xl'}
+                                src={'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'}
+                            />
+                        </Box>
+                        <FormikForm
                             formik={formik}
-                            nameLabel={'name'}
-                            placeHolder={'Update Your Name'} />
-                    </FormikForm>
-                    <Divider my={2} />
+                            color={'green'}
+                            btnTitle={"Update"}
+                        >
+                            <FormikInput
+                                formik={formik}
+                                nameLabel={'name'}
+                                placeHolder={'Update Your Name'} />
+                        </FormikForm>
+                    </>
+                )}
+                <Box>
                     <Heading fontSize={'2xl'} color={'blue.400'} >
                         Orders
                     </Heading>
                     <Divider my={2} />
                     {
-                        state.orders.map(order => <Box key={order?._id} >
-                            <COrderCard order={order} phone={state?.user?.phone} />
-                            <Divider my={2} />
-                        </Box>)
+                        state.user.isAdmin ?
+                            state?.adminOrders?.map(order => <Box key={order?._id} >
+                                <COrderCard order={order} />
+                                <Divider my={2} />
+                            </Box>) :
+                            state.orders.map(order => <Box key={order?._id} >
+                                <COrderCard order={order} phone={state?.user?.phone} />
+                                <Divider my={2} />
+                            </Box>)
                     }
                 </Box>
+
             </Stack>
         </Center>
     )
