44ff0d6027d30b444ca89d7143ff789234eda090
obaidmuneer <obaidmuneer55@gmail.com>
2023-03-26 22:30:02 +0500

    partially removed cache feature

--- public/sw.js
+++ public/sw.js
@@ -31,24 +31,24 @@ workbox.routing.registerRoute(
   })
 );
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith((async () => {
+//       try {
+//         const preloadResp = await event.preloadResponse;
 
-        if (preloadResp) {
-          return preloadResp;
-        }
+//         if (preloadResp) {
+//           return preloadResp;
+//         }
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
+//         const networkResp = await fetch(event.request);
+//         return networkResp;
+//       } catch (error) {
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
+//         const cache = await caches.open(CACHE);
+//         const cachedResp = await cache.match(offlineFallbackPage);
+//         return cachedResp;
+//       }
+//     })());
+//   }
+// });
